2127b2c9cbebfb33e18c78713a59c73afe63db3b
KatharinaMat <katharina.matkevits@gmail.com>
2025-11-28 11:10:12 +0200

    initial commit

---
+++ src/main/java/ee/valiit/mystuffback/MystuffbackApplication.java
@@ -0,0 +1,13 @@
+package ee.valiit.mystuffback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystuffbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystuffbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/valiit/mystuffback/MystuffbackApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.valiit.mystuffback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystuffbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
